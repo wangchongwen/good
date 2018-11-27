faec57df079622248b0b302bdbb5a7d2faa90715
wangchongwen <1291184157@qq.com>
2018-11-27 18:04:59 +0800

    测试

---
+++ src/Good.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Good
+ * Author:   AMITUOFO
+ * Date:     2018/11/27 18:03
+ * Description: 食物
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈食物〉
+ *
+ * @author AMITUOFO
+ * @create 2018/11/27
+ * @since 1.0.0
+ */
+public class Good {
+    public static void main(String[] args){
+      System.out.println("食物");
+    
+    }
+
+}
